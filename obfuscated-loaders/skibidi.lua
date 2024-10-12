1da4c03642d9106df7fb389a447bc04b8f74355e
dario-stuff <hitlerismyfriend123@gmail.com>
2024-10-12 17:46:26 +0200

    Create loader skibidi

---
+++ obfuscated-loaders/skibidi.lua
@@ -0,0 +1,17 @@
+--[[
+
+     _    _ _     _     _ _         _                       
+ ___| | _(_) |__ (_) __| (_)  _ __ / | __ _  __ _  ___ _ __ 
+/ __| |/ / | '_ \| |/ _` | | | '_ \| |/ _` |/ _` |/ _ \ '__|
+\__ \   <| | |_) | | (_| | |_| | | | | (_| | (_| |  __/ |   
+|___/_|\_\_|_.__/|_|\__,_|_(_)_| |_|_|\__, |\__, |\___|_|   
+                                      |___/ |___/           
+
+--> ⚠️ Stop right there! If you are trying to change this code, don't do it, the script will break.
+--> ⚠️ Do not use this directly, use it as a loadstring and with the table.
+
+Do you need the table? Here is the link: https://raw.githubusercontent.com/dario-stuff/scripts/refs/heads/main/tables/skibidi
+
+Put all the way down this as a loadstring for it to work.
+
+--]]
